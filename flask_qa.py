fbf0e71ec2ddb8deb11106bb58c82e1f9974adfe
Sudhanshu <sudhansan5@gmail.com>
2020-12-10 09:11:54 +0530

    login registration form added

--- flask_qa.py
+++ flask_qa.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c240f2ec38f429adc14a1cd05beb0cdc'
 
 ques = [
     {
@@ -21,6 +24,27 @@ ques = [
 @app.route("/home")
 def home():
     return render_template('home.html', ques=ques)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'sudhansan5@gmail.com' and form.password.data == 'abc':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password and try again!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
